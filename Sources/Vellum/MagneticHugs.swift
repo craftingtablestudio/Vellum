cdb9e2396662b9ff40cf041828303934d9f7ced9
Luca Ban <mesqueeb@users.noreply.github.com>
2026-04-29 09:16:43 +1200

    chore: cleanup

---
+++ Sources/Vellum/MagneticHugs.swift
@@ -0,0 +1,10 @@
+extension v0.MagneticHugsComponent: CustomStringConvertible, CustomDebugStringConvertible {
+  public var description: String {
+    let _hugging: String? = if let hugging { "hugging: \(hugging)" } else { nil }
+    let _huggedBy: String? =
+      if !huggedBy.isEmpty { "huggedBy: [\(huggedBy.map { "\($0)" }.join(", "))]" } else { nil }
+    return "MagneticHugsComponent(\([_hugging, _huggedBy].compactMap{$0}.join(", "))"
+  }
+
+  public var debugDescription: String { return self.description }
+}
